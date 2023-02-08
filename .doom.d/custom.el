2bb2ab5db538f3a31a348c08da5c77bf8dbbd3fe
Marvin <nyanjuimarvin@gmail.com>
2023-02-08 12:42:56 +0100

    Add doom Emacs directory and configs

---
+++ .doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/tryout.org"))
+ '(package-selected-packages
+   '(yasnippet-snippets javadoc-lookup highlight-doxygen cmake-project cmake-ide ccls java-snippets company-glsl glsl-mode js-auto-beautify flycheck-clang-tidy markdown-preview-mode markdown-preview-eww flymake-ktlint modern-cpp-font-lock)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
