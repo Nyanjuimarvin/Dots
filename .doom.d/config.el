4c6e662468bd2435cf82c7589de798f39d4b78f3
Marvin <nyanjuimarvin@gmail.com>
2023-03-21 06:55:30 +0100

    Modify doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -33,7 +33,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'catppuccin-macchiato)
+(setq doom-theme 'catppuccin-mocha)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -62,6 +62,15 @@
 (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
 
 (projectile-add-known-project "~/Projects/fuzz")
+
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package all-the-icons
+  :ensure t)
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
